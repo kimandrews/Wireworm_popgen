9dfbb89a06c75eb0138d5b941a764cc83b03a4a5
Kim Andrews <kimandrews@gmail.com>
2021-09-28 13:31:53 -0700

    Update 04-SetupMapping.py

--- RADseq/04-SetupMapping.py
+++ RADseq/04-SetupMapping.py
@@ -8,8 +8,6 @@ bwaIndex = './idx/idx'
 for r1 in glob("./03-superdedup/*_R1.fastq.gz"):
     r2 = r1.replace('_R1.fastq.gz', '_R2.fastq.gz')
     s = r1.split('/')[-1].replace('_R1.fastq.gz', '')
-    if s[0] == 'Z':
-        print r1, s
     cmd = ' '.join(["bwa mem -t 5 -R '@RG\\tID:bwa\\tSM:"+ s +"\\tPL:ILLUMINA'", 
                     bwaIndex, r1, r2, " 2>./04-Mapped/" + s + '.log' + " | samtools view -bS - | samtools sort - > ./04-Mapped/" + s + '.bam',
                     ]) + '\n'
